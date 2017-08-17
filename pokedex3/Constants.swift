28d8f49c22f78cae502240d793d038ff038d5cc5
vincent <myworkplace7@gmail.com>
2017-08-16 21:14:15 -0500

    pokedex final

---
+++ pokedex3/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  pokedex3
+//
+//  Created by Vincent A. Johnson Jr. on 3/31/17.
+//  Copyright © 2017 Vincent A. Johnson Jr. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co/"
+let URL_POKEMON = "api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
+
+
---
+++ pokedex3/PokemonDeatilVC.swift
@@ -0,0 +1,79 @@
+//
+//  PokemonDeatilVC.swift
+//  pokedex3
+//
+//  Created by Vincent A. Johnson Jr. on 3/31/17.
+//  Copyright © 2017 Vincent A. Johnson Jr. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDeatilVC: UIViewController {
+    
+    var pokemon: Pokemon!
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLbl.text = pokemon.name.capitalized
+        
+        let img = UIImage(named: "\(pokemon.pokedexId)")
+        mainImg.image = img
+        currentEvoImg.image = img
+        pokedexLbl.text = "\(pokemon.pokedexId)"
+        
+        pokemon.downloadPokemonDetails {
+            
+           
+            // whatever we write will only be called after the netwirk call is complete!
+            
+            self.updateUI()
+        }
+    }
+    
+    func updateUI() {
+        
+        attackLbl.text = pokemon.attack
+        defenseLbl.text = pokemon.defense
+        heightLbl.text = pokemon.height
+        weightLbl.text = pokemon.weight
+        typeLbl.text = pokemon.type
+        descriptionLbl.text = pokemon.description
+        
+        if pokemon.nextEvolutionId == "" {
+            
+            evoLbl.text = "No Evolutions"
+            nextEvoImg.isHidden = true
+            
+        } else {
+            
+            nextEvoImg.isHidden = false
+            nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
+            let str = "Next Evolution: \(pokemon.nextEvolutionName) - LVL \(pokemon.nextEvolutionLevel)"
+            evoLbl.text = str
+            
+        }
+    }
+
+  
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
